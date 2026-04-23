106b6088716f5bfc1a697d2b0f7365ef0a1663d4
aitugosocial <aitugosocial@gmail.com>
2026-04-23 08:50:44 +0100

    Rebuilding railway

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ set -e
 PORT_TO_USE="${PORT:-8080}"
 
 echo "[start] nginx will listen on port ${PORT_TO_USE}"
-sed "s/__NGINX_PORT__/${PORT_TO_USE}/g" /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
+sed "s/__NGINX_PORT__/${PORT_TO_USE}/g" /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 echo "[start] generated config:"
-cat /etc/nginx/conf.d/default.conf
+cat /etc/nginx/nginx.conf
 echo "[start] testing nginx config..."
 nginx -t
 echo "[start] starting nginx..."
